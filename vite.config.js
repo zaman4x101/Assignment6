5e172ecf599c92522cc609651b8223b977d6013f
zaman4x101 <zaman4x101@gmail.com>
2026-04-14 18:23:28 +0600

    done?

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Assignment6/'  // Important: Your repository name
+  base: '/Assignment6/', // This must match your repository name
 })
